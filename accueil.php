1359680a97fbf8e1bc1ef264d65172bd9c540a8b
Nashikalee <geoffrey.demolon@gmail.com>
2015-02-13 23:58:31 +0100

    changement images

--- accueil.php
+++ accueil.php
@@ -2,11 +2,7 @@
 		<!-- <h1>Accueil</h1> -->
 		<article>
 			<div id="slider">
-				<ul id="content">
-					<li><img src="img/tigre.jpg"></li><!--
-					--><li><img src="img/chat.gif"></li><!--
-					--><li><img src="img/tigre.jpg"></li>
-				</ul>
+				
 			</div>
 		</article>
 	</section>
