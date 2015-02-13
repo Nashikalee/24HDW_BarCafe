e24de5531af861955d3c49703317bad83742b870
Vincent Taing <taing.vincent@gmail.com>
2015-02-13 23:49:36 +0100

    ajout bootstrap

---
+++ menus.php
@@ -0,0 +1,31 @@
+<div>
+	<div class="container">
+		<h2>Les menus</h2>
+		<div class="row">
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+			<div class="col-md-3">
+				<img src="img/duck02.jpg">
+			</div>
+		</div>
+	</div>
+</div>
