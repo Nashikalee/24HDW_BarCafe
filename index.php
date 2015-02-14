219ebc95467af8c338ab5b3ffdc69d5b3efe9f65
Nashikalee <geoffrey.demolon@gmail.com>
2015-02-14 12:57:01 +0100

    image à jour

--- index.php
+++ index.php
@@ -43,9 +43,9 @@
 					</figcaption>
 				</figure>
 				<figure><img src="img/cafe.jpg"></figure>
-				<figure><img src="img/exotique2.jpg"></figure>
+				<figure><img src="img/exotique.jpg"></figure>
 				<figure><img src="img/boutique_avant.jpg"></figure>
-				<figure><img src="img/int2.jpg"></figure>
+				<figure><img src="img/int.jpg"></figure>
 			</div>
 		</article>
 	</section>
