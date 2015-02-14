a61c620fbb2d4653792127574783651613c0394b
Nashikalee <geoffrey.demolon@gmail.com>
2015-02-14 05:12:55 +0100

    slider finit, manque responsive

--- accueil.php
+++ accueil.php
@@ -5,6 +5,8 @@
 					<img src="img/pub2.jpg">
 					<figcaption>
 						<h3>Lorem ipsum dolor sit amet</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.</p>
 					</figcaption>
 				</figure>
 				<figure><img src="img/cafe.jpg"></figure>
